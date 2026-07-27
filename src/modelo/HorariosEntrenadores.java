2a358e4684c3008052824ece451384fb9af5fe00
Erick_Benavides <ed1846020@gmail.com>
2026-07-27 15:01:28 -0500

    Arreglos de paneles y algo mas

--- src/modelo/HorariosEntrenadores.java
+++ src/modelo/HorariosEntrenadores.java
@@ -9,57 +9,64 @@ package modelo;
  * @author erick
  */
 public class HorariosEntrenadores {
-    public class HorarioEntrenador {
+    
 
     private int idHorario;
     private int idEntrenador;
     private String dia;
     private String horaInicio;
     private String horaFin;
+    private String nombreEntrenador;
 
-    public HorarioEntrenador() {
+    public int getIdHorario() {
+        return idHorario;
     }
 
-        public int getIdHorario() {
-            return idHorario;
-        }
+    public void setIdHorario(int idHorario) {
+        this.idHorario = idHorario;
+    }
 
-        public void setIdHorario(int idHorario) {
-            this.idHorario = idHorario;
-        }
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
 
-        public int getIdEntrenador() {
-            return idEntrenador;
-        }
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
 
-        public void setIdEntrenador(int idEntrenador) {
-            this.idEntrenador = idEntrenador;
-        }
+    public String getDia() {
+        return dia;
+    }
 
-        public String getDia() {
-            return dia;
-        }
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
 
-        public void setDia(String dia) {
-            this.dia = dia;
-        }
+    public String getHoraInicio() {
+        return horaInicio;
+    }
 
-        public String getHoraInicio() {
-            return horaInicio;
-        }
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
 
-        public void setHoraInicio(String horaInicio) {
-            this.horaInicio = horaInicio;
-        }
+    public String getHoraFin() {
+        return horaFin;
+    }
 
-        public String getHoraFin() {
-            return horaFin;
-        }
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
+    }
 
-        public void setHoraFin(String horaFin) {
-            this.horaFin = horaFin;
-        }
+    public String getNombreEntrenador() {
+        return nombreEntrenador;
+    }
 
+    public void setNombreEntrenador(String nombreEntrenador) {
+        this.nombreEntrenador = nombreEntrenador;
+    }
+    
+    
     
 }
-}
+
--- src/modelo/PanelHorarios.java
+++ src/modelo/PanelHorarios.java
@@ -4,16 +4,169 @@
  */
 package modelo;
 
+import controlador.ControladorHorario;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import vista.FRMEditarHorario;
+import vista.FRMRegistrarHorario;
 /**
  *
  * @author erick
  */
 public class PanelHorarios extends JPanel {
-    public PanelHorarios() {
+    private JTable tablaHorarioEntrenadores;
+    private JButton btnNuevo;
+    private JButton btnEditar;
+    private JButton btnEliminar;
+    private JButton btnActualizar;
 
-        JLabel titulo = new JLabel("HORARIOS");
-        add(titulo);
+    private JDesktopPane escritorio;
+    public PanelHorarios(JDesktopPane escritorio) {
+
+        this.escritorio = escritorio;
+
+        setLayout(new BorderLayout());
+
+        JLabel titulo = new JLabel("Administración de Horarios de Entrenadores");
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        tablaHorarioEntrenadores = new JTable();
+
+        tablaHorarioEntrenadores.setModel(new DefaultTableModel(
+                new Object[][]{},
+                new String[]{
+                    "ID",
+                    "Entrenador",
+                    "Dia",
+                    "Hora Inicio",
+                    "Hora Fin"
+                }
+        ));
+        JScrollPane scroll = new JScrollPane(tablaHorarioEntrenadores);
+
+        JPanel panelBotones = new JPanel(new FlowLayout());
+
+        btnNuevo = new JButton("Nuevo");
+        btnEditar = new JButton("Editar");
+        btnEliminar = new JButton("Eliminar");
+        btnActualizar = new JButton("Actualizar");
+
+        panelBotones.add(btnNuevo);
+        panelBotones.add(btnEditar);
+        panelBotones.add(btnEliminar);
+        panelBotones.add(btnActualizar);
+
+        add(titulo, BorderLayout.NORTH);
+        add(scroll, BorderLayout.CENTER);
+        add(panelBotones, BorderLayout.SOUTH);
+
+        cargarHorarios();
+        
+        btnNuevo.addActionListener(e -> {
+
+            FRMRegistrarHorario frm = new FRMRegistrarHorario();
+
+            escritorio.add(frm);
+
+            frm.setVisible(true);
+        });
+        
+        btnEditar.addActionListener(e -> {
+
+            int fila = tablaHorarioEntrenadores.getSelectedRow();
+
+            if (fila == -1) {
+
+                JOptionPane.showMessageDialog(this,
+                        "Seleccione un entrenador.");
+
+                return;
+            }
+
+            int id = (int) tablaHorarioEntrenadores.getValueAt(fila, 0);
+
+            ControladorHorario controlador = new ControladorHorario();
+
+            HorariosEntrenadores horario = controlador.buscarHorario(id);
+
+            FRMEditarHorario frm = new FRMEditarHorario(horario);
+
+            escritorio.add(frm);
+
+            frm.setVisible(true);
+
+        });
+        btnEliminar.addActionListener(e -> {
+
+            int fila = tablaHorarioEntrenadores.getSelectedRow();
+
+            if (fila == -1) {
+
+                JOptionPane.showMessageDialog(this,
+                        "Seleccione un horario.");
+
+                return;
+            }
+
+            int opcion = JOptionPane.showConfirmDialog(
+                    this,
+                    "¿Desea eliminar este horario?",
+                    "Confirmar",
+                    JOptionPane.YES_NO_OPTION);
+
+            if (opcion == JOptionPane.YES_OPTION) {
+
+                int id = (int) tablaHorarioEntrenadores.getValueAt(fila, 0);
+
+                ControladorHorario controlador = new ControladorHorario();
+
+                if (controlador.eliminarHorario(id)) {
+
+                    JOptionPane.showMessageDialog(this,
+                            "Horario eliminado correctamente.");
+
+                    cargarHorarios();
+
+                } else {
+
+                    JOptionPane.showMessageDialog(this,
+                            "No se pudo eliminar el Horario.");
+                }
+            }
+
+        });
+        
+        btnActualizar.addActionListener(e -> {
+            cargarHorarios();
+        });
     }
+    public void cargarHorarios() {
+
+    ControladorHorario controlador = new ControladorHorario();
+
+    DefaultTableModel modelo =
+            (DefaultTableModel) tablaHorarioEntrenadores.getModel();
+
+    modelo.setRowCount(0);
+
+    for (HorariosEntrenadores h : controlador.listarHorarios()) {
+
+        modelo.addRow(new Object[]{
+            h.getIdHorario(),
+            h.getNombreEntrenador(),
+            h.getDia(),
+            h.getHoraInicio(),
+            h.getHoraFin()
+        });
+    }
+}
 }
